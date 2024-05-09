06aa550bed283ec6b80cbb303d9f917281fb19a1
ChengZhiNB <292200693@qq.com>
2024-05-09 21:38:55 +0800

    提交程序

--- public/assets/NotFound-WNlaMnqk.js
+++
@@ -1,1 +0,0 @@
-import{c as t,o,a as s}from"./index-CbE64-_3.js";const e="/assets/ERROR-lIE7qfxy.png",c={class:"container"},n=s("div",{class:"Title"},[s("div",null,[s("img",{alt:"",src:e}),s("h2",null,"发生了错误QAQ")])],-1),a=s("div",{class:"Info"},[s("h4",null,"原因: 找不到你要寻找的页面")],-1),_=[n,a],p={__name:"NotFound",setup(l){return(i,r)=>(o(),t("div",c,_))}};export{p as default};
